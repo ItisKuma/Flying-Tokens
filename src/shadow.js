6f32a227639d3ec7be8a7e5670155f97d1635899
ItisKuma <Rasmus.johs@gmail.com>
2026-04-02 17:11:40 +0200

    Update shadow.js

--- src/shadow.js
+++ src/shadow.js
@@ -1,9 +1,13 @@
 import OBR from "https://unpkg.com/@owlbear-rodeo/sdk?module";
 
+// Opret shadow (clone af token)
 export async function createShadow(token) {
+
   const shadow = structuredClone(token);
+
   shadow.id = crypto.randomUUID();
 
+  // Gør den mørk og transparent
   shadow.opacity = 0.3;
   shadow.tint = "#000000";
 
@@ -12,6 +16,8 @@ export async function createShadow(token) {
   return shadow.id;
 }
 
+
+// Fjern shadow
 export async function removeShadow(id) {
   if (!id) return;
   await OBR.scene.items.deleteItems([id]);
