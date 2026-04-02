4f24dfdf4f46ff663571dfb8256b5d7bea4b15a8
ItsKuma <Rasmus.johs@gmail.com>
2026-04-02 21:50:03 +0200

    initial

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import OBR from "@owlbear-rodeo/sdk";
 import { toggleFlyingState, setZ } from "./flying.js";
 import { rebuildFlyingList } from "./list.js";
@@ -6,20 +5,6 @@ import { rebuildFlyingList } from "./list.js";
 OBR.onReady(() => {
 
   document.getElementById("toggle").onclick = async () => {
-=======
-import OBR from "https://unpkg.com/@owlbear-rodeo/sdk?module";
-
-import { toggleFlyingState, setZ } from "./flying.js";
-import { rebuildFlyingList } from "./list.js";
-
-const ID = "simple-flying";
-
-OBR.onReady(() => {
-
-  //  Toggle Flying
-  document.getElementById("toggle").onclick = async () => {
-
->>>>>>> 663484910b30287bb66b34542176094f2176fa07
     const selection = await OBR.player.getSelection();
     const id = selection[0];
     if (!id) return;
@@ -27,18 +12,11 @@ OBR.onReady(() => {
     await toggleFlyingState(id);
   };
 
-<<<<<<< HEAD
   document.getElementById("z").oninput = async (e) => {
-=======
-  //  Z slider
-  document.getElementById("z").oninput = async (e) => {
-
->>>>>>> 663484910b30287bb66b34542176094f2176fa07
     const selection = await OBR.player.getSelection();
     const id = selection[0];
     if (!id) return;
 
-<<<<<<< HEAD
     await setZ(id, Number(e.target.value));
   };
 
@@ -46,17 +24,3 @@ OBR.onReady(() => {
 
   rebuildFlyingList();
 });
-=======
-    const z = Number(e.target.value);
-    await setZ(id, z);
-  };
-
-  //  Global sync (det her er nøglen)
-  OBR.scene.items.onChange(async () => {
-    await rebuildFlyingList();
-  });
-
-  // Initial load
-  rebuildFlyingList();
-});
->>>>>>> 663484910b30287bb66b34542176094f2176fa07
