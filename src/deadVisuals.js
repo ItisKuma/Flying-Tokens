4fe57b45fda1749208080d7b1fa0e28cbe4d80f0
ItsKuma <Rasmus.johs@gmail.com>
2026-04-30 22:18:08 +0200

    Update deadVisuals.js

--- src/deadVisuals.js
+++ src/deadVisuals.js
@@ -7,7 +7,7 @@ export const LOCAL_DEAD_NS = `${NS}-local-dead`;
 const DEAD_VISUAL_ID_PREFIX = `${NS}/dead-visual/`;
 const DEAD_ANIMATION_DURATION_MS = 480;
 const DEAD_SPLAT_SIZE_MULTIPLIER = 3;
-const DEAD_SPLAT_Y_OFFSET_RATIO = 0.18;
+const DEAD_SPLAT_Y_OFFSET_RATIO = 0.08;
 const BLOOD_GRID = {
   dpi: 150,
   offset: { x: 0, y: 0 },
@@ -96,8 +96,8 @@ function getDeadVisualPosition(item, bounds, size) {
   const yOffset = size.height * DEAD_SPLAT_Y_OFFSET_RATIO;
 
   return {
-    x: center.x - size.width / 2,
-    y: center.y - size.height / 2 + yOffset,
+    x: center.x,
+    y: center.y + yOffset,
   };
 }
 
