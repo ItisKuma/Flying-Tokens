8dab567c9b4bdd98e7d0ed768689ab582f544f79
ItsKuma <Rasmus.johs@gmail.com>
2026-05-19 06:33:32 +0200

    Update deadVisuals.js

--- src/deadVisuals.js
+++ src/deadVisuals.js
@@ -14,21 +14,11 @@ const BLOOD_GRID = {
 const EXTENSION_ORIGIN = globalThis.location?.origin ?? "";
 const BLOOD_SPLAT_IDS = [
   "blood_splat_01.png",
-  "blood_splat_02.png",
-  "blood_splat_03.png",
   "blood_splat_04.png",
   "blood_splat_05.png",
-  "blood_splat_06.png",
-  "blood_splat_07.png",
   "blood_splat_08.png",
   "blood_splat_09.png",
-  "blood_splat_10.png",
-  "blood_splat_11.png",
-  "blood_splat_12.png",
   "blood_splat_13.png",
-  "blood_splat_14.png",
-  "blood_splat_15.png",
-  "blood_splat_16.png",
   "blood_splat_17.png",
 ];
 let cachedRolePromise = null;
