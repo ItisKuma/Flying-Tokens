7b739a157e233ff4cac3771eddb2de88639ecfe5
ItsKuma <Rasmus.johs@gmail.com>
2026-05-22 15:24:31 +0200

    Update blood_splat_02.png, blood_splat_03.png, blood_splat_04.png and more

--- src/deadSplats.js
+++ src/deadSplats.js
@@ -1,16 +1,16 @@
 export const BLOOD_SPLATS = [
-  { file: "blood_splat_02.png", width: 311, height: 296, offsetSquaresX: 0, offsetSquaresY: 0 },
-  { file: "blood_splat_03.png", width: 304, height: 279, offsetSquaresX: 0.6, offsetSquaresY: -0.6 },
-  { file: "blood_splat_04.png", width: 272, height: 279, offsetSquaresX: 0, offsetSquaresY: 0 },
-  { file: "blood_splat_05.png", width: 302, height: 250, offsetSquaresX: 0, offsetSquaresY: 0 },
-  { file: "blood_splat_06.png", width: 295, height: 258, offsetSquaresX: 0.5, offsetSquaresY: -0.5 },
-  { file: "blood_splat_07.png", width: 313, height: 287, offsetSquaresX: 0, offsetSquaresY: 0 },
-  { file: "blood_splat_08.png", width: 297, height: 256, offsetSquaresX: 1, offsetSquaresY: -0.5 },
-  { file: "blood_splat_09.png", width: 289, height: 252, offsetSquaresX: 0, offsetSquaresY: -0.5 },
-  { file: "blood_splat_10.png", width: 313, height: 282, offsetSquaresX: 0.85, offsetSquaresY: -0.55 },
-  { file: "blood_splat_11.png", width: 310, height: 268, offsetSquaresX: 0.5, offsetSquaresY: -0.2 },
-  { file: "blood_splat_13.png", width: 294, height: 274, offsetSquaresX: 0.15, offsetSquaresY: -0.55 },
-  { file: "blood_splat_14.png", width: 288, height: 282, offsetSquaresX: 0.3, offsetSquaresY: -0.1 },
+  { file: "blood_splat_02.png", width: 295, height: 305, offsetSquaresX: 0, offsetSquaresY: 0 },
+  { file: "blood_splat_03.png", width: 308, height: 296, offsetSquaresX: 0.6, offsetSquaresY: -0.6 },
+  { file: "blood_splat_04.png", width: 326, height: 289, offsetSquaresX: 0, offsetSquaresY: 0 },
+  { file: "blood_splat_05.png", width: 321, height: 280, offsetSquaresX: 0, offsetSquaresY: 0 },
+  { file: "blood_splat_06.png", width: 306, height: 303, offsetSquaresX: 0.5, offsetSquaresY: -0.5 },
+  { file: "blood_splat_07.png", width: 294, height: 294, offsetSquaresX: 0, offsetSquaresY: 0 },
+  { file: "blood_splat_08.png", width: 328, height: 335, offsetSquaresX: 0.7, offsetSquaresY: -0.5 },
+  { file: "blood_splat_09.png", width: 294, height: 362, offsetSquaresX: 0, offsetSquaresY: -0.5 },
+  { file: "blood_splat_10.png", width: 318, height: 356, offsetSquaresX: 0.85, offsetSquaresY: -0.55 },
+  { file: "blood_splat_11.png", width: 315, height: 230, offsetSquaresX: 0.5, offsetSquaresY: -0.2 },
+  { file: "blood_splat_13.png", width: 324, height: 280, offsetSquaresX: 0.15, offsetSquaresY: -0.55 },
+  { file: "blood_splat_14.png", width: 294, height: 279, offsetSquaresX: 0.3, offsetSquaresY: -0.1 },
 ];
 
 export const BLOOD_SPLAT_IDS = BLOOD_SPLATS.map((splat) => splat.file);
