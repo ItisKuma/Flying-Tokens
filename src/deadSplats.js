9223171d87213d571f2d5e81b5376f459b42d352
ItsKuma <Rasmus.johs@gmail.com>
2026-05-22 14:46:03 +0200

    Update deadSplats.js

--- src/deadSplats.js
+++ src/deadSplats.js
@@ -5,10 +5,10 @@ export const BLOOD_SPLATS = [
   { file: "blood_splat_05.png", width: 302, height: 250, offsetSquaresX: 0, offsetSquaresY: 0 },
   { file: "blood_splat_06.png", width: 295, height: 258, offsetSquaresX: 0.5, offsetSquaresY: -0.5 },
   { file: "blood_splat_07.png", width: 313, height: 287, offsetSquaresX: 0, offsetSquaresY: 0 },
-  { file: "blood_splat_08.png", width: 297, height: 256, offsetSquaresX: 0, offsetSquaresY: 0 },
+  { file: "blood_splat_08.png", width: 297, height: 256, offsetSquaresX: 1, offsetSquaresY: -0.5 },
   { file: "blood_splat_09.png", width: 289, height: 252, offsetSquaresX: 0, offsetSquaresY: -0.5 },
   { file: "blood_splat_10.png", width: 313, height: 282, offsetSquaresX: 0.85, offsetSquaresY: -0.55 },
-  { file: "blood_splat_11.png", width: 310, height: 268, offsetSquaresX: 1, offsetSquaresY: -0.2 },
+  { file: "blood_splat_11.png", width: 310, height: 268, offsetSquaresX: 0.5, offsetSquaresY: -0.2 },
   { file: "blood_splat_13.png", width: 294, height: 274, offsetSquaresX: 0.15, offsetSquaresY: -0.55 },
   { file: "blood_splat_14.png", width: 288, height: 282, offsetSquaresX: 0.3, offsetSquaresY: -0.1 },
 ];
